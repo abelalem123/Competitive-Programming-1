03c9c046e584a508e7d776cc38526f48dd9fe084
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2022-10-16 22:41:17 +0300

    Time: 6473 ms (11.15%), Space: 22 MB (6.36%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,20 +1,22 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        dp = [[False] * n for _ in range(n)]
-        for i in range(n):
-            dp[i][i] = True
-        longest_palindrome_start, longest_palindrome_len = 0, 1
-
-        for end in range(0, n):
-            for start in range(end - 1, -1, -1):
-                # print('start: %s, end: %s' % (start, end))
-                if s[start] == s[end]:
-                    if end - start == 1 or dp[start + 1][end - 1]:
-                        dp[start][end] = True
-                        palindrome_len = end - start + 1
-                        if longest_palindrome_len < palindrome_len:
-                            longest_palindrome_start = start
-                            longest_palindrome_len = palindrome_len
-        return s[longest_palindrome_start: longest_palindrome_start + longest_palindrome_len]
+        start=0
+        end=0
+        dp=[[False]*len(s) for _ in range(len(s))]
+        for i in range(len(s)-1,-1,-1):
+            dp[i][i]=True
+            # if i-1>=0:
+            #     if s[i]==s[i-1]:
+            #         dp[i-1][i]=1
+            #         start=i-1
+            #         end=i
+        for i in range(len(s)-1,-1,-1):
+            for j in range(i+1,len(s)):
+                if s[i]==s[j]:
+                    if j-i==1 or dp[i+1][j-1]:
+                        dp[i][j]=True
+                        if j-i>=end-start:#if length is greater than current update pointers
+                            start=i
+                            end=j
+        return s[start:end+1]
      
