11fa3057751a75921d268d63e230381a89362c23
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2022-10-18 12:02:34 +0300

    Time: 413 ms (21.36%), Space: 13.9 MB (93.20%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -2,17 +2,14 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         if not len(s):
             return 0
-        alph='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        win={i:0 for i in alph}
-        #ABAB
         longest=0
         l,r=0,0
+        win={}
         for r in range(len(s)):
-            win[s[r]]+=1
-            maxc=max(win.values())#this will take o(26)#1
-            if k<((r-l+1)-maxc):
-                if win[s[l]]!=0:
-                    win[s[l]]-=1
+            win[s[r]]=1+win.get(s[r],0)
+            maxc=max(win.values())#this will take o(26)
+            while k<((r-l+1)-maxc):
+                win[s[l]]-=1
                 l+=1
             longest=max(r-l+1,longest)
         return longest
