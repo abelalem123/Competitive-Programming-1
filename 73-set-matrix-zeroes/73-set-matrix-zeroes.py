e414cfadfde90e2e1b3b438ffa5d908f41a2c463
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2022-10-21 21:26:57 +0300

    Time: 358 ms (13.46%), Space: 14.9 MB (16.33%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -8,13 +8,30 @@ class Solution:
         num=2**31 + 1
         row=len(matrix)
         col=len(matrix[0])
+        row0=False
+        col0=False
         for i in range(row):
-            for j in range(col):
+            if matrix[i][0]==0:
+                col0=True
+                break
+        for j in range(col):
+            if matrix[0][j]==0:
+                row0=True
+                break
+        for i in range(1,row):
+            for j in range(1,col):
                 if matrix[i][j]==0:
-                    rowset.add(i)
-                    colset.add(j)
-        for i in range(row):
-            for j in range(col):
-                if i in rowset or j in colset:
+                    matrix[i][0]=0
+                    matrix[0][j]=0
+
+        for i in range(1,row):
+            for j in range(1,col):
+                if matrix[i][0]==0 or matrix[0][j]==0:
                     matrix[i][j]=0
+        if col0:
+            for i in range(row):
+                matrix[i][0]=0
+        if row0:
+            for j in range(col):
+                matrix[0][j]=0
             
