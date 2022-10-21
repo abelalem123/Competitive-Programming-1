a5311b25790e2b89a8c33269d72455ffb925c185
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2022-10-21 23:21:23 +0300

    Time: 83 ms (7.62%), Space: 14 MB (25.51%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,28 +1,28 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        #q=collections.deque()
-        res=''
+        res=[]
         i=len(a)-1
         j=len(b)-1
         carry=0
         while i>=0 and j>=0:
-            temp=(int(a[i]) + int(b[j]) + carry)
-            carry=(temp)//2
-            res=str(temp%2)+res
+            temp=int(a[i]) + int(b[j]) + carry
+            carry=temp//2
+            res.append(str(temp%2))
             
             i-=1
             j-=1
         while i>=0:
             temp=int(a[i])+carry
             carry=temp//2
-            res=str(temp%2)+res
+            res.append(str(temp%2))
             i-=1
         while j>=0:
             temp=int(b[j])+carry
             carry=temp//2
-            res=str(temp%2)+res
+            res.append(str(temp%2))
             j-=1
         if carry:
-            res=str(carry)+res
-        return res
+            res.append(str(carry))
+        res.reverse()
+        return ''.join(res)
             
