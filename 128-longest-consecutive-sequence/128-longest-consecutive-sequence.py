76469383d1daf606ca8054f3643210dca0e45aae
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2022-10-22 17:31:04 +0300

    Time: 928 ms (51.77%), Space: 29 MB (61.59%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -7,14 +7,11 @@ class Solution:
                 continue
             else:
                 starting.add(i)
-        for i in starting:
-            numset.remove(i)
         max_count=0
         for i in starting:
             nextn=i+1
             count=1
             while (nextn in numset):
-                numset.remove(nextn)
                 count+=1
                 nextn+=1
             max_count=max(count,max_count)
