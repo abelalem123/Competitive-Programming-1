03cf84f45dc4863042d25daa8251530ecd620fcf
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2022-10-26 17:16:54 +0300

    Time: 82 ms (97.69%), Space: 14.3 MB (81.03%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -1,13 +1,16 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        dpmax=[0 for i in range(len(nums))]
-        dpmin=[math.inf for i in range(len(nums))]
-        dpmin[0]=nums[0]
-        dpmax[0]=nums[0]
-        for i in range(1,len(dpmin)):
-                dpmax[i]=max(nums[i]*dpmin[i-1],nums[i]*dpmax[i-1],nums[i])
-                #print(dpmax[1])
-                dpmin[i]=min(nums[i]*dpmin[i-1],nums[i]*dpmax[i-1],nums[i])
-                #print(dpmax)
-                
-        return max(dpmax)
+        #dpmax=[0 for i in range(len(nums))]
+        #dpmin=[math.inf for i in range(len(nums))]
+        upto_max=1
+        upto_min=1
+        #dpmin[0]=nums[0]
+        #dpmax[0]=nums[0]
+        absolute_max=nums[0]
+        for num in nums:
+                tempmin=num*upto_min
+                tempmax=num*upto_max
+                upto_max=max(tempmin,tempmax,num)
+                upto_min=min(tempmin,tempmax,num)
+                absolute_max=max(absolute_max,upto_max)
+        return absolute_max
