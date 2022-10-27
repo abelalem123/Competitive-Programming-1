aa8976272a5a3143681f65c8da0ba1b9108f7a12
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2022-10-27 19:01:16 +0300

    Time: 52 ms (56.26%), Space: 13.9 MB (62.84%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -9,15 +9,13 @@ class Solution:
             mid=l+(r-l)//2
             q=isBadVersion(mid)
             #print("left",l,"right",r,"mid",mid)
-            if q and (l+1==mid or l==mid):
-                return l if isBadVersion(l) else mid
-                
-            elif q:
+            # if q and (l+1==mid or l==mid):
+            #     return l if isBadVersion(l) else mid
+            if q:
                 r=mid-1
             else:
                 l=mid+1
         return l 
-        #print(mid, l, r)
-        #return 
+
     
             
