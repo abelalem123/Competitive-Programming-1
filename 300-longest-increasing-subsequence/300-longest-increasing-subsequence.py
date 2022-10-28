04e06570e69ed00d6088987bbff6e8615adf7867
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2022-10-28 15:39:22 +0300

    Time: 3021 ms (64.58%), Space: 14.3 MB (44.71%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,13 +1,11 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp=[-math.inf for i in range(len(nums))] # dp represents the number of sequence upto this interval
-        dp[0]=1
+        dp=[1 for i in range(len(nums))] # dp represents the number of sequence upto this interval
+        count=1
         for i in range(1,len(dp)):
-            dp[i]=1
             for j in range(0,i):
                 if nums[i]>nums[j]:
-                    
                     if dp[j]+1>dp[i]:
                         dp[i]=1+dp[j]
-        print(dp)
-        return max(dp)
+                        count=max(count,dp[i])
+        return count
