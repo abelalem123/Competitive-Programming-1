8860c57854e806f7d6926b2fc0803750fba97c53
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2022-11-12 17:25:35 +0300

    Time: 4158 ms (19.41%), Space: 28.5 MB (34.94%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -1,10 +1,22 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        i = 0
+
+        #slidding window
+        #condition to expand the window
+        #if k plus sum of elements in the window is greater than 
+        #the number at the right times length of the window, why??
+        #because it tells us that if all the numbers were all the same as the right
+        #the sum would have been just that
+        l = 0
         nums.sort()
-        for j in range(len(nums)):
-            k += nums[j]
-            if k < nums[j] * (j - i + 1):
-                k -= nums[i]
-                i += 1
-        return j - i + 1
+        total=0
+        maxf=0
+        for r,n in enumerate(nums):
+            #sum of elements in the window
+            total+=n
+            #condition to shrink the window
+            while k+total<n*(r-l+1):
+                total-=nums[l]
+                l+=1
+            maxf=max(maxf,r-l+1)
+        return maxf
