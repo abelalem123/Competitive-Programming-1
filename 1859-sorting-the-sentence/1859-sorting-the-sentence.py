df7310c9d5c91964c9914bc76127878cf09f1247
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2022-11-13 19:08:48 +0300

    Time: 44 ms (75.80%), Space: 13.9 MB (11.40%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,8 +1,15 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        sen=s.split(' ')
-        li=sorted(sen, key=lambda w:w[-1])
-        res=[]
-        for i in li:
-            res.append(i[:-1])
-        return ' '.join(res)
+        # sen=s.split(' ')
+        # li=sorted(sen, key=lambda w:w[-1])
+        # res=[]
+        # for i in li:
+        #     res.append(i[:-1])
+        # return ' '.join(res)
+        
+        li=s.split(' ')
+        result=['']*len(li)
+        for word in li:
+            index=int(word[-1])
+            result[index-1]=word[:-1]
+        return ' '.join(result)
