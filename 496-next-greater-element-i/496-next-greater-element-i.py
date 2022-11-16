363da8ab1c38a421557496c67ba39bf2c02e96ac
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2022-11-16 13:47:39 +0300

    Time: 93 ms (68.61%), Space: 14.3 MB (14.38%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,13 +1,22 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         hashmap={i:-1 for i in nums1}
-        res=[]
+        stack=[]
+        
         for i,num in enumerate(nums2):
-            temp=-1
+            while stack and num>stack[-1]:
+                temp=stack.pop()
+                hashmap[temp]=num
             if num in hashmap:
-                for j in range(i,len(nums2)):
-                    if nums2[j]>num:
-                        temp=nums2[j]
-                        break
-                hashmap[num]=temp
+                stack.append(num)
         return hashmap.values()
+        #naive solution o(m*n)
+        # for i,num in enumerate(nums2):
+        #     temp=-1
+        #     if num in hashmap:
+        #         for j in range(i,len(nums2)):
+        #             if nums2[j]>num:
+        #                 temp=nums2[j]
+        #                 break
+        #         hashmap[num]=temp
+        # return hashmap.values()
