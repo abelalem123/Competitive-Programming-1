6bac38dce01b9f098aa0dfe2aee31eceaca158d4
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2022-12-30 09:46:39 +0300

    Time: 40 ms (77.95%), Space: 14 MB (30.49%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,11 +1,18 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        sfreq={}
-        tfreq={}
+        # sfreq={}
+        # tfreq={}
+        # for i in s:
+        #     sfreq[i]=sfreq.get(i,0)+1
+        # for i in t:
+        #     tfreq[i]=tfreq.get(i,0)+1
+        # for k,v in tfreq.items():
+        #     if k not in sfreq or v!=sfreq[k]:
+        #         return k
+        #bitwise solution
+        cup=0
         for i in s:
-            sfreq[i]=sfreq.get(i,0)+1
+            cup^=ord(i)
         for i in t:
-            tfreq[i]=tfreq.get(i,0)+1
-        for k,v in tfreq.items():
-            if k not in sfreq or v!=sfreq[k]:
-                return k
+            cup^=ord(i)
+        return chr(cup)
