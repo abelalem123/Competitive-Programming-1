b97050b936242889e4c9e3a9b2448134167aafde
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2022-12-30 09:37:43 +0300

    Time: 31 ms (95.70%), Space: 13.8 MB (74.35%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        sfreq={}
+        tfreq={}
+        for i in s:
+            sfreq[i]=sfreq.get(i,0)+1
+        for i in t:
+            tfreq[i]=tfreq.get(i,0)+1
+        for k,v in tfreq.items():
+            if k not in sfreq or v!=sfreq[k]:
+                return k
