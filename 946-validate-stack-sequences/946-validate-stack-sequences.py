599b0f14c554bab6b1d5c5e9fb2d2de1cb035875
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-01-02 18:55:17 +0300

    Time: 75 ms (85.27%), Space: 14.2 MB (45.75%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.py
+++ 946-validate-stack-sequences/946-validate-stack-sequences.py
@@ -3,25 +3,21 @@ class Solution:
         pu=0
         po=0
         st=[]
-        while po<len(popped) and pu<len(popped):
+        while po<len(popped) and pu<len(pushed):
             if st and st[-1]==popped[po]:
                 st.pop()
                 po+=1
             else:
                 st.append(pushed[pu])
                 pu+=1
-        while pu<len(pushed):
-            #print('here push')
-            st.push()
-            pu+=1
         while po<len(popped):
-            #print(st,popped)
             if st and st[-1]==popped[po]:
                 st.pop()
                 po+=1
             else:
                 return False
 
-        return pu==len(pushed) and po==len(popped)
+
+        return po==len(popped) and pu==len(pushed)
                 
                 
