ff2751deaed2e65c485553f52f556f067da699fe
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-01-04 23:25:17 +0300

    Time: 227 ms (27.26%), Space: 14.1 MB (9.41%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -2,20 +2,19 @@ class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         #pointers for the two lists given
         p2=len(num2)-1
-        sumn=0
+        sumn=-1
         mult=[]
         while p2>=0:
             result = self.multiply_by_one_digit(num2[p2],num1)
             print(result)
             p2-=1
-            if sumn==0:
-                sumn=result
+            if sumn==-1:
+                sumn=int(result)
                 continue
 
             power=len(num2)-2-p2
-            sumn=str(int(result)*10**power + int(sumn))
-    
-        return str(int(sumn))
+            sumn=int(result)*10**power + sumn   
+        return str(sumn)
     
     def multiply_by_one_digit(self,digit,num):
         p1=len(num)-1
