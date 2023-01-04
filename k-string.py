431e819e3beefd23dc2b14b9ff23e51ec81b9b97
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-01-04 22:02:17 +0300

    Update k-string.py

--- k-string.py
+++ k-string.py
@@ -1,23 +1,16 @@
+def kstring():
+	k=int(input())
+	string=input()
+	mp={}
+	for i in string:
+	    mp[i]=mp.get(i,0)+1
+	res=[]
 
-k=int(input())
-string=input()
-mp={}
-for i in string:
-    mp[i]=mp.get(i,0)+1
-res=[]
-flag=True
-#a:2,b:2,c:2
-#we want 2-string
-for key,value in mp.items():
-    if value%k==0:
-        x=[key]*(value//k) #build the k string i.e a*2//2 a*1
-    else:
-        flag=False
-        break
-    res.extend(x) #build the first segment of the k-string e.g abc
-new=[]
-if not flag:
-    print("-1")
-else:
-    new.extend(res*k) #build the whole segment by multiplying abc*k e.g abcabc
-    print(''.join(new))
+	for key,value in mp.items():
+	    if value%k==0:
+		x=key*(value//k) 
+	    else:
+		return '-1'
+	    res.append(x) 
+	return ''.join(res*k) #build the whole segment by multiplying abc*k e.g abcabc
+print(kstring())
