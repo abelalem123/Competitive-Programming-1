16272d826ffa963a9052306787c75d9bcb07f0c0
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-01-05 18:08:37 +0300

    Time: 2313 ms (10.97%), Space: 39.3 MB (66.06%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -1,20 +1,48 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        current=head
-        a=[]
-        while current:
-            a.append(current.val) 
-            current=current.next
-        l=0
-        r=len(a)-1
-        while l<r:
-            if a[l]!=a[r]:
+        """
+        1->2->2->1
+              l   r
+        find middle
+        reverse the right part to 1->2
+        compare and return bool (1->2 and 1->2)
+        
+        """
+        dummy = ListNode(0,next=head)
+        left = head
+        right = dummy #to slow down right pointer because we want left to be before middle
+        
+        # find the middle of the list
+        while right.next and right.next.next:
+            left = left.next
+            right = right.next.next
+        
+        # reverse the right part of the list
+        reversed_node = self.reverse_list(left)
+        left = head
+        
+        # compare the values to determine if palindrom
+        while left and reversed_node:
+            if left.val != reversed_node.val:
                 return False
-            l+=1
-            r-=1
+            left = left.next
+            reversed_node = reversed_node.next
         return True
+    
+    def reverse_list(self,node):
+        next=None
+        prev=None
+        current=node
+        while current:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        return prev
+            
+    
