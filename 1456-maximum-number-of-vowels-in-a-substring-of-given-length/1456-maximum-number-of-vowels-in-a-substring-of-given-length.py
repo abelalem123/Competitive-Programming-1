0348ec124147d5ac3578c7a4763e340a2b62aad9
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-01-07 23:37:51 +0300

    Time: 162 ms (93.69%), Space: 14.7 MB (77.57%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -4,29 +4,35 @@ class Solution:
         l =0
         vowels = {'a','e','i','o','u'}
         vowel_buck ={}
+        count = 0
         for i in s[:k]:
             if i in vowels:
-                vowel_buck[i] = vowel_buck.get(i,0) + 1
+                count +=1
+                #vowel_buck[i] = vowel_buck.get(i,0) + 1
             # bucket[i] = bucket.get(i,0) + 1
-        max_vowel=sum(vowel_buck.values())
-        if max_vowel == k:
-            return max_vowel
+        max_vowel=count
+        if count == k:
+            return count
 
         for r in range(k,len(s)):
             # if s[l] in bucket:
             #     bucket[s[l]]-=1
             #     if bucket[s[l]]==0:
             #         del bucket[s[l]]
-            if s[l] in vowel_buck:
-                vowel_buck[s[l]] -=1
-                if vowel_buck[s[l]] == 0:
-                    del vowel_buck[s[l]]
+            if s[l] in vowels:
+                count -=1
+                if count <0:
+                    count = 0
+                # vowel_buck[s[l]] -=1
+                # if vowel_buck[s[l]] == 0:
+                #     del vowel_buck[s[l]]
             
                 
             if s[r] in vowels:
-                vowel_buck[s[r]] = vowel_buck.get(s[r],0) + 1
+                count +=1
+                #vowel_buck[s[r]] = vowel_buck.get(s[r],0) + 1
             #bucket[s[r]] = bucket.get(i,0) + 1
-            
-            max_vowel = max(max_vowel, sum(vowel_buck.values()))
+            max_vowel = max(max_vowel,count)
+            #max_vowel = max(max_vowel, sum(vowel_buck.values()))
             l+=1
         return max_vowel
