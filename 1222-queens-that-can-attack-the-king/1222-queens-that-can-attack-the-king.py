44aad5bc18944647c82ef9b4758c2097c43ff6f9
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-01-13 02:01:33 +0300

    Time: 44 ms (78.17%), Space: 14.1 MB (18.60%) - LeetHub

---
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
@@ -0,0 +1,90 @@
+class Solution:
+    def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]:
+        table=set(tuple(i) for i in queens)
+        good_queens=self.find_nearest_queens(king[0],king[1],table)
+        result=[]
+        for queen_row, queen_col in good_queens:
+            king_row=king[0]
+            king_col=king[1]
+
+            #horizontal
+            if king_row==queen_row:
+                result.append((queen_row,queen_col))
+                continue
+                
+            #vertical
+            elif king_col==queen_col:
+                result.append((queen_row,queen_col))
+                continue
+                
+            #diagonal
+
+            elif  (abs(king_row - queen_row) == abs(king_col - queen_col)):
+                result.append((queen_row,queen_col))
+                continue
+        return result
+
+    def find_nearest_queens(self,king_row,king_col,table):
+        good_queens =set()
+        #left
+        for i in range(king_col-1,-1,-1):
+            if (king_row,i) in table:
+                good_queens.add((king_row,i))
+                break
+        #right
+        for i in range(king_col+1,8):
+            if (king_row,i) in table:
+                good_queens.add((king_row,i))
+                break
+        #up
+        for i in range(king_row-1,-1,-1):
+            if (i,king_col) in table:
+                good_queens.add((i,king_col))
+                break
+        #down
+        for i in range(king_row+1,8):
+            if (i,king_col) in table:
+                good_queens.add((i,king_col))
+                break
+    
+        #left_up
+        r=king_row - 1
+        c=king_col - 1
+        while r>=0 or c>=0:
+            if (r,c) in table:
+                good_queens.add((r,c))
+                break
+            r-=1
+            c-=1
+
+        #right_up
+        r=king_row - 1
+        c=king_col + 1
+        while r>=0 or c<=7:
+            if (r,c) in table:
+                good_queens.add((r,c))
+                break
+            r-=1
+            c+=1
+
+        #left_down
+        r=king_row + 1
+        c=king_col - 1
+        while r<=7 or c>=0:
+            if (r,c) in table:
+                good_queens.add((r,c))
+                break
+            r+=1
+            c-=1
+        
+        #right_down
+        r=king_row + 1
+        c=king_col + 1
+        while r<=7 or c<=7:
+            if (r,c) in table:
+                good_queens.add((r,c))
+                break
+            r+=1
+            c+=1
+    
+        return good_queens        
