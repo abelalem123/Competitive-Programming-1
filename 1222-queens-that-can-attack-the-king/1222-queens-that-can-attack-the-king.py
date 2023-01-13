40a6c58231141af4171fc9f73b91f7ea2a660008
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-01-14 00:01:06 +0300

    Time: 42 ms (84.06%), Space: 14.1 MB (17.99%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
@@ -2,7 +2,19 @@ class Solution:
     def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]:
         table=set(tuple(i) for i in queens)
         good_queens=self.find_nearest_queens(king[0],king[1],table)
-        return good_queens
+        directions = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(-1,-1),(1,-1),(-1,1)]
+        x_int=king[0]
+        y_int=king[1]
+        result=[]
+        for dx,dy in directions:
+            for i in range(1,8):
+                    x=x_int + i*dx
+                    y=y_int + i*dy
+                    if (0<=x<=7 and 0<=y<=7) and (x,y) in table:
+                        result.append((x,y))
+                        break
+
+        return result
 
     def find_nearest_queens(self,king_row,king_col,table):
         good_queens =set()
