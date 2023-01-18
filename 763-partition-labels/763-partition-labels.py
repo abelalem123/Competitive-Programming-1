80f49e811e95f8aa5e2764c7e33ac520d612e4e7
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-01-18 23:31:39 +0300

    Time: 44 ms (69.88%), Space: 13.9 MB (62.54%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -4,23 +4,39 @@ class Solution:
         ababcba
         ab a:1 b:
         """
-        remaining=Counter(s)
-        win_words=set()
-        parts=[]
-        l=0
-        length=0
-        for r,c in enumerate(s):
-            win_words.add(c)
-            if c in remaining:
-                remaining[c]-=1
-                if remaining[c]==0:
-                    del remaining[c]
-            flag=True
-            for i in win_words:
-                flag=flag and i not in remaining
+#         remaining=Counter(s)
+#         win_words=set()
+#         parts=[]
+#         l=0
+#         length=0
+#         for r,c in enumerate(s):
+#             win_words.add(c)
+#             if c in remaining:
+#                 remaining[c]-=1
+#                 if remaining[c]==0:
+#                     del remaining[c]
+#             flag=True
+#             for i in win_words:
+#                 flag=flag and i not in remaining
                     
-            if flag: #if our window is unique elements at our window
-                parts.append(r-l+1)
-                l=r+1
+#             if flag: #if our window is unique elements at our window
+#                 parts.append(r-l+1)
+#                 l=r+1
                 
-        return parts
+#         return parts
+#another solution
+        l=0 #left pointer
+        last_pos={} 
+        max_pos=-math.inf # the maximum of positions in our window
+        part=[] #length of partions
+        
+        #compute last position
+        for i,c in enumerate(s):
+            last_pos[c]=i
+        
+        for r,c in enumerate(s):
+            max_pos=max(max_pos,last_pos[c])
+            if max_pos==r:
+                part.append(r-l+1)
+                l=r+1
+        return part
