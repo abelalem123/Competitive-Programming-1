106b4be943f92d448dd1b74956b0168e6462252e
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-01-23 15:08:13 +0300

    Time: 1952 ms (54.02%), Space: 35.9 MB (9.41%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -7,10 +7,9 @@ class Solution:
         #first window
         for r in range(k):
             num=nums[r]
-            #win[num]=win.get(num,0) + 1
             max_heap.append(-num)
-            #max_num = max(max_num,num)
         heapq.heapify(max_heap)
+        
         sub_max.append(-1*max_heap[0])
         left = 0 #left pointer
         to_be_removed ={}
@@ -18,18 +17,16 @@ class Solution:
         for right in range(k,len(nums)):
             num_right=nums[right]
             num_left=nums[left]
-            left+=1
+            
             to_be_removed[num_left]=1 + to_be_removed.get(num_left,0)
-            #print(to_be_removed)
+            left+=1
+            heapq.heappush(max_heap,-num_right)
             while max_heap and -1*max_heap[0] in to_be_removed:
                 
                 temp=-1*heapq.heappop(max_heap)
-                
-                #print(temp)
                 to_be_removed[temp]-=1
                 if to_be_removed[temp] == 0:
                     del to_be_removed[temp]
-            heapq.heappush(max_heap,-num_right)
             sub_max.append(-1*max_heap[0])
         return sub_max
             
