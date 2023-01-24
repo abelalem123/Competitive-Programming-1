258d8327e50227c1b828461fcd9b3f5b03edc620
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-01-24 22:08:18 +0300

    Time: 92 ms (58.33%), Space: 14.1 MB (28.07%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
@@ -4,37 +4,30 @@ class Solution:
         consider two case:
         1. firstlen comes before secondlen maintain max sum of firstlen and move secondlen  as a window
         [0,6,5,2,2,5,1,9,4]
-           _ ___    _ 
+           ____    _ 
         2. firstlen comes after secondlen, so maintain maximum sum of secondlen and move firstlen as a window
         
         [1 2 3 4 5 6]
          - --- 
         """
+        firstlen_being_former = self.max_sum(nums,firstLen,secondLen)
+        secondlen_being_former = self.max_sum(nums,secondLen,firstLen)
         
-        firstlen_cur = sum(nums[:firstLen])
-        secondlen_win = sum(nums[firstLen:firstLen + secondLen])
-        left = 0
-        firstlen_max = firstlen_cur
-        result = firstlen_max + secondlen_win
-        #window 1,where we maintain maximum sum of firstlen that appears before secondlen, and we move secondlen as a window
-        for i in range(firstLen + secondLen,len(nums)):
-            firstlen_cur += nums[i-secondLen] - nums[i-firstLen - secondLen]
-            secondlen_win += nums[i] - nums[i-secondLen]
-            left+=1
+        return max(firstlen_being_former, secondlen_being_former)
+        
+    def max_sum(self,nums,former:int,later:int) -> int:
+        
+        total_len = former + later 
+        former_current_sum = sum(nums[:former])
+        later_current_sum = sum(nums[former: total_len])
+        former_max_sum =former_current_sum 
+        result = former_max_sum + later_current_sum
+        
+        for i in range(total_len, len(nums)):
+            former_current_sum += nums[i-later] - nums[i-total_len]
+            later_current_sum += nums[i] - nums[i-later] 
+            former_max_sum= max(former_max_sum, former_current_sum)
+            result = max(result, former_max_sum + later_current_sum)
             
-            firstlen_max = max(firstlen_max,firstlen_cur)
-            result = max(result,firstlen_max + secondlen_win)
-
-
-        secondlen_cur = sum(nums[:secondLen])
-        firstlen_win = sum(nums[secondLen: firstLen + secondLen])
-        left = 0
-        secondlen_max =secondlen_cur #initialize our sum for secondlen
-        
-        for i in range(firstLen + secondLen, len(nums)):
-            secondlen_cur += nums[i-firstLen] - nums[i-firstLen - secondLen]
-            firstlen_win += nums[i] - nums[i-firstLen] 
-            left +=1
-            secondlen_max= max(secondlen_max, secondlen_cur)
-            result = max(result, secondlen_max + firstlen_win)
         return result
+            
