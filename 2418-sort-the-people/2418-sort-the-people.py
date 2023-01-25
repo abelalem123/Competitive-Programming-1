918dd5715ffb32ef830b26d3fd1ad0842bdcc972
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-01-25 21:57:21 +0300

    Time: 952 ms (18.68%), Space: 17.5 MB (7.61%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -18,10 +18,23 @@ class Solution:
         #     names[i],names[max_index]=names[max_index],names[i]
         
         #insertion sort
-        for i in range(1,len(names)):
-            j=i
-            while j and heights[j] > heights[j-1]:
-                heights[j],heights[j-1]=heights[j-1],heights[j]
-                names[j],names[j-1]=names[j-1],names[j]  
-                j-=1
+        # for i in range(1,len(names)):
+        #     j=i
+        #     while j and heights[j] > heights[j-1]:
+        #         heights[j],heights[j-1]=heights[j-1],heights[j]
+        #         names[j],names[j-1]=names[j-1],names[j]  
+        #         j-=1
+        
+        #counting sort
+        height_name_idx ={height:name for height,name  in zip(heights,names)}
+        min_num = min(heights)
+        max_num = max(heights)
+        count = [0 for i in range(min_num + max_num +1)]
+        for h in heights:
+            count[h + min_num]+=1
+        pointer = len(heights)-1
+        for i,n in enumerate(count):
+            for c in range(n):
+                names[pointer]= height_name_idx[i - min_num]
+                pointer -=1
         return names
