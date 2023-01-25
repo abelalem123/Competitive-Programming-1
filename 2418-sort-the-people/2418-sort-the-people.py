26a24bfd47053f632e1a6295462f8f6d0987b705
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2023-01-25 21:14:08 +0300

    Time: 2189 ms (6.24%), Space: 14.4 MB (80.73%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,6 +1,7 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         
+        #bubble sort        
 #         for i in range(len(names)-1,-1,-1):
 #             for j in range(1,i+1):
                 
@@ -8,9 +9,19 @@ class Solution:
 #                     heights[j],heights[j-1]=heights[j-1],heights[j]
 #                     names[j],names[j-1]=names[j-1],names[j]
 #         #print(heights)
-        for i in range(len(names)):
-            max_num=max(heights[i:])
-            max_index=heights.index(max_num)
-            heights[i],heights[max_index]=heights[max_index],heights[i]
-            names[i],names[max_index]=names[max_index],names[i]
+
+        #selection sort
+        # for i in range(len(names)):
+        #     max_num=max(heights[i:])
+        #     max_index=heights.index(max_num)
+        #     heights[i],heights[max_index]=heights[max_index],heights[i]
+        #     names[i],names[max_index]=names[max_index],names[i]
+        
+        #insertion sort
+        for i in range(1,len(names)):
+            j=i
+            while j and heights[j] > heights[j-1]:
+                heights[j],heights[j-1]=heights[j-1],heights[j]
+                names[j],names[j-1]=names[j-1],names[j]  
+                j-=1
         return names
